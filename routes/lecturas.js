7f0708397ec1b5df3676c52ede22a8d658f5e0c2
dfaparicio <dfapariciocastro13@gmail.com>
2025-12-06 15:29:51 -0500

    Inicio de proyecto NodeMongo

---
+++ routes/lecturas.js
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import {
+  generarlecturaprincipal,
+  generarlecturadiaria,
+  obtenerlecturasdeunusuario,
+  obtenerlecturaporid,
+} from "../controllers/lecturascontrollers.js";
+
+const router = Router();
+
+router.post("/api/lecturas/principal/:usuario_id", generarlecturaprincipal);
+
+router.post("/api/lecturas/diaria/:usuario_id", generarlecturadiaria);
+
+router.get("/api/lecturas/usuario/:usuario_id", obtenerlecturasdeunusuario);
+
+router.get("/api/lecturas/:id", obtenerlecturaporid);
+
+export default router;
