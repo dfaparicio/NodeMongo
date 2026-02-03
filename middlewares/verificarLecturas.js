4ec337e3529e92b4ba6abcece172b754f32ddaa0
dfaparicio <dapariciocastro13@gmail.com>
2026-02-03 18:51:24 -0500

    Correccion de lecturas

--- middlewares/verificarLecturas.js
+++ middlewares/verificarLecturas.js
@@ -1,23 +1,5 @@
 import Lectura from "../models/lecturas.js";
 
-export const verificarLectura = (req, res, next) => {
-  if (!req.body) {
-    return res.status(400).json({
-      msg: "El cuerpo de la petición (body) es obligatorio",
-    });
-  }
-
-  const { contenido } = req.body;
-
-  if (!contenido || contenido.trim() === "") {
-    return res.status(400).json({
-      msg: "El contenido es obligatorio",
-    });
-  }
-
-  next();
-};
-
 export const verificarLecturaPrincipal = async (req, res, next) => {
   const { usuarioId } = req.params;
 
