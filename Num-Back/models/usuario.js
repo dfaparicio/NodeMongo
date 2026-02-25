9e13297045817596cc7198380dd705a25b03af44
andft <andrescarvajal1415@gmail.com>
2026-02-24 22:18:51 -0500

    se agregaron enpoind de envio de email

--- Num-Back/models/usuario.js
+++ Num-Back/models/usuario.js
@@ -1,13 +1,16 @@
 import mongoose from "mongoose";
 
 const usuario = new mongoose.Schema({
-    nombre:{type:String,required:true},
-    edad:{type:Number},
-    fechanacimiento:{type:Date, default:Date.now},
-    email:{type:String,unique:true, required: true},
-    password:{type:String, required: true},
-    rol:{type:String, required:true, default:'USER_ROLE', enum:['ADMIN_ROLE', 'USER_ROLE', 'OTHER_ROLE']},
-    estado:{type:Number,default:1}//0 inactivo   1 activo
+    nombre: { type: String, required: true },
+    edad: { type: Number },
+    fechanacimiento: { type: Date, default: Date.now },
+    email: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
+    rol: { type: String, required: true, default: 'USER_ROLE', enum: ['ADMIN_ROLE', 'USER_ROLE', 'OTHER_ROLE'] },
+    estado: { type: Number, default: 1 },//0 inactivo   1 activo
+    emailVerificado: { type: Boolean, default: false },
+    resetToken: { type: String, default: null },
+    resetTokenExpira: { type: Date, default: null },
 });
 
-export default mongoose.model("Usuario",usuario)
+export default mongoose.model("Usuario", usuario)
