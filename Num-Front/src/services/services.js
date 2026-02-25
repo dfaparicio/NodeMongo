629aa6e9d2605042d2a64b57bcd2b39334f96581
Diego Fernando Aparicio Castro <dapariciocastro13@gmail.com>
2026-02-25 13:06:24 -0500

    Login, Store, Dashboard

--- Num-Front/src/services/services.js
+++ Num-Front/src/services/services.js
@@ -1,6 +1,25 @@
 import axiosInstance from "../plugins/pluginAxios.js";
 
+// GET: Para obtener datos (ej: el perfil)
+export const getData = async (url) => {
+  const response = await axiosInstance.get(url);
+  return response.data; 
+};
+
+// POST
 export const postData = async (url, data) => {
   const response = await axiosInstance.post(url, data);
-  return response;
+  return response.data;
+};
+
+// PUT
+export const putData = async (url, data) => {
+  const response = await axiosInstance.put(url, data);
+  return response.data;
+};
+
+// DELETE
+export const deleteData = async (url) => {
+  const response = await axiosInstance.delete(url);
+  return response.data;
 };
--- Num-Front/src/store/auth.js
+++ Num-Front/src/store/auth.js
@@ -1,13 +1,46 @@
 import { defineStore } from "pinia";
 import { ref } from "vue";
+import { computed } from "vue";
 
 export const useAuthStore = defineStore(
   "auth",
   () => {
     let token = ref("");
+    const user = ref(null);
+
+    const fechanacimiento = computed(() => {
+      if (!user.value?.fechanacimiento) return "--";
+
+      const fecha = new Date(user.value.fechanacimiento);
+
+      const meses = [
+        "Ene",
+        "Feb",
+        "Mar",
+        "Abr",
+        "May",
+        "Jun",
+        "Jul",
+        "Ago",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dic",
+      ];
+
+      return `${fecha.getDate()} ${meses[fecha.getMonth()]} ${fecha.getFullYear()}`;
+    });
+
+    const logout = () => {
+      token.value = "";
+      user.value = null;
+    };
 
     return {
       token,
+      user,
+      logout,
+      fechanacimiento,
     };
   },
 
