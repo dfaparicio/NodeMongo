43efb31f934101cdfe71edf4361add03d4d79e59
andft <andrescarvajal1415@gmail.com>
2026-02-25 20:30:11 -0500

    cambios

--- Num-Back/models/usuario.js
+++ Num-Back/models/usuario.js
@@ -7,7 +7,7 @@ const usuario = new mongoose.Schema({
     email: { type: String, unique: true, required: true },
     password: { type: String, required: true },
     rol: { type: String, required: true, default: 'USER_ROLE', enum: ['ADMIN_ROLE', 'USER_ROLE', 'OTHER_ROLE'] },
-    estado: { type: Number, default: 1 },//0 inactivo   1 activo
+    estado: { type: Number, default: 0 },//0 inactivo   1 activo
     emailVerificado: { type: Boolean, default: false },
     resetToken: { type: String, default: null },
     resetTokenExpira: { type: Date, default: null },
