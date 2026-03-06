d7befce180c43ac11ad86ac6d8be31c4440524e1
Diego Fernando Aparicio Castro <dapariciocastro13@gmail.com>
2026-03-06 01:56:52 -0500

    Pruebas

--- Num-Back/controllers/lecturas.js
+++ Num-Back/controllers/lecturas.js
@@ -149,7 +149,7 @@ export const generarlecturaprincipal = async (req, res) => {
 
 export const generarlecturadiaria = () => {
   cron.schedule(
-    "50 01 * * *",
+    "50 02 * * *",
     async () => {
       console.log("⏰ [Cron] Generando lecturas diarias para usuarios activos...");
 
