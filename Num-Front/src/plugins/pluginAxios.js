65d426d8fd25a2d210190745b29cf655d0bd3640
Diego Fernando Aparicio Castro <dapariciocastro13@gmail.com>
2026-03-17 22:27:51 -0500

    Correcciones generales 5

--- Num-Front/src/plugins/pluginAxios.js
+++ Num-Front/src/plugins/pluginAxios.js
@@ -2,8 +2,8 @@ import axios from "axios";
 import { useAuthStore } from "../store/auth.js";
 
 const axiosInstance = axios.create({
-  // Forzamos ruta relativa para que funcione siempre en producción (Render)
-  baseURL: "/api",
+  // Si hay una variable de entorno la usa, si no, usa /api (que sirve para Render)
+  baseURL: import.meta.env.VITE_API_URL || "/api",
   headers: {
     "Content-Type": "application/json",
   },
