5bb31403bd479633ae34552488675d36fba69302
Diego Fernando Aparicio Castro <dapariciocastro13@gmail.com>
2026-03-17 22:15:19 -0500

    Correcciones generales 4

--- Num-Front/src/plugins/pluginAxios.js
+++ Num-Front/src/plugins/pluginAxios.js
@@ -2,7 +2,8 @@ import axios from "axios";
 import { useAuthStore } from "../store/auth.js";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "/api",
+  // Forzamos ruta relativa para que funcione siempre en producción (Render)
+  baseURL: "/api",
   headers: {
     "Content-Type": "application/json",
   },
