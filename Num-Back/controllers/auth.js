c513f9f9bbfc2b23e5c0764e7a03c27f8005f17a
Diego Fernando Aparicio Castro <dapariciocastro13@gmail.com>
2026-04-21 08:21:43 -0500

    Pruebas con Git 4

--- Num-Back/controllers/auth.js
+++ Num-Back/controllers/auth.js
@@ -67,7 +67,7 @@ export const login = async (req, res) => {
             })
         }
         const validPassword = bcrypt.compareSync(password, usuario.password)
-        if (!validPasswor) {
+        if (!validPassword) {
             return res.status(401).json({
                 error: "Credenciales incorrectas"
             })
