928f5f7bde7d439b8523d1330fa5e21188e7f571
Diego Fernando Aparicio Castro <dapariciocastro13@gmail.com>
2026-04-28 10:04:55 -0500

    Correcciones y explicacion

--- Num-Front/__tests__/mocks/fileMock.js
+++ Num-Front/__tests__/mocks/fileMock.js
@@ -1,8 +1,5 @@
 /**
  * Mock para archivos de imagen (jpg, jpeg, png, gif, svg)
- *
- * Cuando Jest encuentre una importación de una imagen,
- * usará este archivo en su lugar.
  */
 
 export default 'mock-file';
--- Num-Front/__tests__/utils/formateo.test.js
+++
@@ -1,7 +0,0 @@
-describe('PRUEBAS FRONTEND - UTILS', () => {
-  test('Debe limpiar guiones de la fecha para el envío', () => {
-    const input = '1995-07-12';
-    const output = input.replace(/-/g, "");
-    expect(output).toBe('19950712');
-  });
-});
